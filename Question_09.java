3611f0f565f5cb447d399fad24ec33d4b367678e
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2022-03-24 23:30:32 +0530

    Question09

---
+++ Question_09.java
@@ -0,0 +1,16 @@
+
+public class Question_09 {
+	public static void main(String[]args)
+	{
+		int num= Integer.parseInt(args[0]);
+		System.out.println("Developer: D4_Pratik_Satpute_62758");
+		System.out.println("Java Test");
+		System.out.println("Enter Number: "+num);
+		System.out.println("given number: " +num );
+		System.out.println("Binary equivalent: " +Integer.toBinaryString(num));
+		System.out.println("Octal equivalent: " + Integer.toOctalString(num));
+		System.out.println("hexadecimal equovalent: " + Integer.toHexString(num));
+	
+	}
+
+}
