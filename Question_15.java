3a5f2d161e3bbbc565f1200db42788c7d1671ca6
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2022-03-25 23:54:14 +0530

    Question 12

--- Question_15.java
+++ Question_15.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 /* Write a function to check if string is palindrome. The Function should 
 return True or False. You may use built-in functions, if required*/
-public class Question_15 {
-	public static void main(String[]args)
-	{	
+class palindrome
+{
+	 void fun()
+	{
 		String str2="";
-		System.out.println("Enter the word to check for Palindrome ");
+		System.out.println("Enter word to check for palindrome ");
 		Scanner sc=new Scanner (System.in);
 		String str1 =sc.next();
 		int n = str1.length();
@@ -21,24 +22,17 @@ public class Question_15 {
 		else 
 			System.out.println("False");
 		
-		
-		
-		/*String str2="";
-		System.out.println("Enter the word to check for Palindrome ");
-		Scanner sc=new Scanner (System.in);
-		String str1 =sc.next();
-		int n = str1.length();
-		for(int i=n-1;i>=0;i--)
-			{
-			str2 = str2+ str1.charAt(i);
-			}
-		if(str1.equalsIgnoreCase(str2)) 
-		{
-			System.out.println("The word "+str1+ " is Palindrome");
-		}
-		else 
-			System.out.println("The word is "+str1+ " not Palindrome");
-		*/
 	}
-
 }
+public class Question_15 
+	{
+	public static void main(String[]args)
+	{	
+		System.out.println("Developer: D4_Pratik_Satpute_62758");
+		palindrome pal = new palindrome();
+		pal.fun();
+	}
+	}
+	
+
+
